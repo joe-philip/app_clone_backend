51be5f732ead975b36e9d4df30714513958bd40c
Joe Philip <joe.mj007@gmail.com>
2025-02-23 07:22:35 +0000

    defined LogEntry model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from root.app_utils.meta_models.meta import MetaModel
+
+
+class LogEntry(MetaModel):
+    user = models.ForeignKey(
+        'auth.User',
+        related_name='logs',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        db_table = 'log_entry'
+        verbose_name = 'Log entry'
+        verbose_name_plural = 'Log entries'
+
+    def __str__(self) -> str: return self.user.username

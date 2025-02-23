196480acf29c5973fa48a51317a431746cba11a7
Joe Philip <joe.mj007@gmail.com>
2025-02-23 06:25:58 +0000

    added exception hander to settings

--- root/settings.py
+++ root/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'root.app_utils.exception_handling.exception_handler.exception_handler'
+}
